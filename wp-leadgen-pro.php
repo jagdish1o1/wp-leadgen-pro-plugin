989a6212ae09586fc4165fd274c2c4ce1ddcd816
Jagdish <nackedblogging@gmail.com>
2024-07-17 22:16:14 +0530

    updating version

--- wp-leadgen-pro.php
+++ wp-leadgen-pro.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Leadgen Pro
  * Description: Plugin to integration with wp leadgen pro platform.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: WP Leadgen Pro
  * Author URI: https://wpleadgen.pro
  * License: GPLv2 or later
